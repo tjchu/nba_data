e948643c4096fe2470da4c593ba3ac77ff8589dd
Terry Chu <terrychu2002@gmail.com>
2019-03-29 16:42:27 -0700

    Update player_info.py

--- player_info.py
+++ player_info.py
@@ -6,16 +6,32 @@ import sys
 
 def find_player(name):
 	player_search = players.find_players_by_full_name(name)
+	plural = ""
 	if len(player_search) == 0:
 		print("No player with name '%s' found." % name)
-		sys.exit(0) 
-	player_id = player_search[0]['id']
-	full_name = player_search[0]['full_name']
-	player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()
-	school = player_info['CommonPlayerInfo'][0]['SCHOOL']
-	print("Player with name '%s' found! Here is some information about him:" % name)
-	print("\tFull Name: %s" % full_name)
-	print("\tSchool: %s" % school)
+		sys.exit(0)
+	if len(player_search) > 1:
+		plural = "s"
+	print("Found player%s matching the name '%s'" % (plural, name)) 
+	for player in player_search:
+		player_id = player['id']
+		player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()
+
+		# Player Information
+		full_name = player['full_name']
+		school = player_info['CommonPlayerInfo'][0]['SCHOOL']
+		if '-' in player_info['CommonPlayerInfo'][0]['HEIGHT']:
+			height = player_info['CommonPlayerInfo'][0]['HEIGHT'].split('-')
+			height = "%s'%s''" % (height[0], height[1])
+		else: 
+			height = "N/A"
+
+		print("----")
+		print("Player with name '%s' found! Here is some information about him:" % name)
+		print("\tFull Name: %s" % full_name)
+		print("\tSchool: %s" % school)
+		print("\tHeight: %s" % height)
+		print("----")
 
 """
 lebron_info = commonplayerinfo.CommonPlayerInfo(player_id=2544)
